db12623350d7f1696dbba665c19b1cc7f758d276
Akshay Mishra <akshaymishra94@gmail.com>
2024-12-14 20:23:34 +0530

    table ceation

--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -4,9 +4,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity(name="user")
 @Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
 
     @Id
