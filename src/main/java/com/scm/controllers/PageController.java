a9f919653c0e33790395d5f09361c2e469c249ca
Akshay Mishra <akshaymishra94@gmail.com>
2024-12-28 19:15:47 +0530

    registration message removal and provider enumeration

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -11,7 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.scm.entities.User;
 import com.scm.forms.UserForm;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
 import com.scm.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
@@ -73,14 +77,23 @@ public class PageController {
 
     //processing register form
     @RequestMapping(value = "/do-register", method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm)
+    public String processRegister(@ModelAttribute UserForm userForm, HttpSession session)
     {
         System.out.println("Processing registration");
         System.out.println(userForm);
-        User user = User.builder().name(userForm.getName()).email(userForm.getEmail()).password(userForm.getPassword()).
-        about(userForm.getAbout()).phoneNumber(userForm.getPhoneNumber()).build();
+        //User user = User.builder().name(userForm.getName()).email(userForm.getEmail()).password(userForm.getPassword()).
+        //about(userForm.getAbout()).phoneNumber(userForm.getPhoneNumber()).build();
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+
         User saveduser = userService.saveUser(user);
         System.out.println("User saved");
+        Message message = Message.builder().content("Registration successfull").type(MessageType.green).build();
+        session.setAttribute("message", message);
         return "redirect:/register";
     }
     
